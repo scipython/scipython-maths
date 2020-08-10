466d36f43a6aab6e3ce4d5d06e8627443f128a8a
Christian Hill <xn.hill@gmail.com>
2020-08-10 09:55:19 +0200

    Update df_maze.py
    Remove example usage code

--- maze/df_maze.py
+++ maze/df_maze.py
@@ -163,21 +163,3 @@ class Maze:
             cell_stack.append(current_cell)
             current_cell = next_cell
             nv += 1
-
-
-"""
-# Example usage:
-
-from df_maze import *
-
-# Maze dimensions (ncols, nrows)
-nx, ny = 15, 15
-# Maze entry position
-ix, iy = 0, 0
-
-maze = Maze(nx, ny, ix, iy)
-maze.make_maze()
-
-print(maze)
-maze.write_svg('maze.svg')
-"""
