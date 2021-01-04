b99eaae31b6bc151ebc4346c2eaf29965027cde6
Christian Hill <xn.hill@gmail.com>
2021-01-04 10:36:13 +0100

    Update Lorenz attractor script

--- lorenz/lorenz.py
+++ lorenz/lorenz.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -7,15 +7,18 @@ from mpl_toolkits.mplot3d import Axes3D
 # The maths behind this code is described in the scipython blog article
 # at https://scipython.com/blog/the-lorenz-attractor/
 # Christian Hill, January 2016.
+# Updated, January 2021 to use scipy.integrate.solve_ivp.
 
-# Lorenz paramters and initial conditions
+WIDTH, HEIGHT, DPI = 1000, 750, 100
+
+# Lorenz paramters and initial conditions.
 sigma, beta, rho = 10, 2.667, 28
 u0, v0, w0 = 0, 1, 1.05
 
-# Maximum time point and total number of time points
+# Maximum time point and total number of time points.
 tmax, n = 100, 10000
 
-def lorenz(X, t, sigma, beta, rho):
+def lorenz(t, X, sigma, beta, rho):
     """The Lorenz equations."""
     u, v, w = X
     up = -sigma*(u - v)
@@ -23,24 +26,28 @@ def lorenz(X, t, sigma, beta, rho):
     wp = -beta*w + u*v
     return up, vp, wp
 
-# Integrate the Lorenz equations on the time grid t
+# Integrate the Lorenz equations.
+soln = solve_ivp(lorenz, (0, tmax), (u0, v0, w0), args=(sigma, beta, rho),
+                 dense_output=True)
+# Interpolate solution onto the time grid, t.
 t = np.linspace(0, tmax, n)
-f = odeint(lorenz, (u0, v0, w0), t, args=(sigma, beta, rho))
-x, y, z = f.T
+x, y, z = soln.sol(t)
 
-# Plot the Lorenz attractor using a Matplotlib 3D projection
-fig = plt.figure()
+# Plot the Lorenz attractor using a Matplotlib 3D projection.
+fig = plt.figure(facecolor='k', figsize=(WIDTH/DPI, HEIGHT/DPI))
 ax = fig.gca(projection='3d')
+ax.set_facecolor('k')
+fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
 # Make the line multi-coloured by plotting it in segments of length s which
 # change in colour across the whole time series.
 s = 10
-c = np.linspace(0,1,n)
+cmap = plt.cm.winter
 for i in range(0,n-s,s):
-    ax.plot(x[i:i+s+1], y[i:i+s+1], z[i:i+s+1], color=(1,c[i],0), alpha=0.4)
+    ax.plot(x[i:i+s+1], y[i:i+s+1], z[i:i+s+1], color=cmap(i/n), alpha=0.4)
 
 # Remove all the axis clutter, leaving just the curve.
 ax.set_axis_off()
 
-plt.savefig('lorenz.png')
+plt.savefig('lorenz.png', dpi=DPI)
 plt.show()
